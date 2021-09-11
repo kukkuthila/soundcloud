950b53cb3f24fc4737d17139ae7cdee2d7b4e4fb
kukkuthila <87352930+kukkuthila@users.noreply.github.com>
2021-09-11 09:26:30 +0530

    Add files via upload

---
+++ soundcloud/main.js
@@ -0,0 +1,22 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+SC.stream('/tracks/231652579',function(sound){
+
+  $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+/tracks/1112490034
+/tracks/176348833
+});
+});
